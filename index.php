d298330ac139b0324f5b4742f8cc65f8bcff1aea
omid <99048099@mydavinci.nl>
2023-06-19 10:24:19 +0200

    "index.php: Simuleert een spel met de klassen uit Pokemon.php."
    index.php: Dit bestand maakt gebruik van de klassen gedefinieerd in Pokemon.php om een spel te simuleren. Het maakt nieuwe objecten van de klassen Pokemon, Attack, Weakness en Resistance, stelt ze in interactie door aanvallen uit te voeren, en drukt uiteindelijk informatie uit over de huidige staat van de Pokémon.
    Het bevat ook de functie printPokemonStats(), die de huidige staat van een Pokémon-object afdrukt, inclusief zijn naam, gezondheid, zwakke punten, weerstanden en aanvallen.
    
    Let op: In de vorige versies van de code was er een fout waar de functie printPokemonStats() meerdere keren werd gedefinieerd. Dit veroorzaakte een fatale fout. In de herziene code is deze functie slechts één keer gedefinieerd in index.php, waardoor de fout is verholpen.

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
 <?php
 
-require_once 'Pikachu.php';
-require_once 'Charmeleon.php';
+require 'Pokemon.php';
 
-$pikachu = new Pikachu('Pikachu');
-$charmeleon = new Charmeleon('Charmeleon');
+function printPokemonStats($pokemon)
+{
+    echo "Name: " . $pokemon->name . "<br>";
+    echo "Health: " . $pokemon->health . "/" . $pokemon->hitpoints . "<br>";
+    echo "Weakness: " . $pokemon->weakness->energyType . " " . $pokemon->weakness->multiplier . "<br>";
+    echo "Resistance: " . $pokemon->resistance->energyType . " " . $pokemon->resistance->value . "<br>";
+    echo "Attack(s): ";
+    foreach ($pokemon->attacks as $attack)
+    {
+        echo $attack->name . " " . $attack->damage . ", ";
+    }
+    echo "<br><br>";
+}
 
-$pikachu->printStats();
-$charmeleon->printStats();
+$pikachu = new Pokemon("Pikachu", "Lightning", 60,
+    [new Attack("Electric Ring", 50), new Attack("Pika Punch", 20)],
+    new Weakness("Fire", 1.5), new Resistance("Fighting", 20)
+);
+$charmeleon = new Pokemon("Charmeleon", "Fire", 60,
+    [new Attack("Head Butt", 10), new Attack("Flare", 30)],
+    new Weakness("Water", 2), new Resistance("Lightning", 10)
+);
 
-$pikachu->attack($pikachu->getAttacks()[0], $charmeleon);
-$charmeleon->attack($charmeleon->getAttacks()[0], $pikachu);
+printPokemonStats($pikachu);
+printPokemonStats($charmeleon);
 
-echo "Number of pokemons alive: 2";
+$pikachu->attack($charmeleon, $pikachu->attacks[0]);
+echo "Pikachu attacked Charmeleon and dealt " . $pikachu->attacks[0]->damage . " damage<br>";
+echo "Charmeleon has " . $charmeleon->health . " health left<br><br>";
+
+$charmeleon->attack($pikachu, $charmeleon->attacks[0]);
+echo "Charmeleon attacked Pikachu and dealt " . $charmeleon->attacks[0]->damage . " damage<br>";
+echo "Pikachu has " . $pikachu->health . " health left<br><br>";
+
+echo "Number of pokemons alive: " . Pokemon::getPopulation();
+
 ?>
